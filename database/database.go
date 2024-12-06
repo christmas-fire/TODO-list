ab6544a7ecba393b5b33ffe6ea3886aff32d7c24
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2024-12-06 22:45:25 +0700

    Реализовал подключение к PostgreSQL. В дальнейшем прописать параметры подключения в конфиг или подгружать их из .env

---
+++ database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func InitDB() *sql.DB {
+	connStr := "host=localhost port=5432 user=postgres password=290605 dbname=todo_list sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
+	}
+
+	// Проверяем соединение
+	if err := db.Ping(); err != nil {
+		log.Fatalf("База данных недоступна: %v", err)
+	}
+
+	log.Println("Успешное подключение к базе данных")
+	return db
+}
