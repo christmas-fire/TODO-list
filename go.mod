31a3d6718104076e8438a40d67b6d5300d4e5565
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2024-12-07 13:44:07 +0700

    Реализовал REST API

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module todo_list
 
 go 1.23.0
 
+require github.com/lib/pq v1.10.9
+
 require (
-	github.com/lib/pq v1.10.9
+	github.com/gorilla/mux v1.8.1
 )
--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"todo_list/database"
 	"todo_list/tasks"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -18,13 +20,19 @@ func main() {
 	db = database.InitDB()
 	defer db.Close()
 
-	http.HandleFunc("/tasks/get", GetAllTasksHandler)
-	http.HandleFunc("/tasks/put", AddTaskHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/tasks", GetAllTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id:[0-9]+}", GetTaskByIDHandler).Methods("GET")
+	r.HandleFunc("/tasks", AddTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id:[0-9]+}", UpdateTaskHandler).Methods("PATCH")
+	r.HandleFunc("/tasks/{id:[0-9]+}", DeleteTaskHandler).Methods("DELETE")
 
 	fmt.Println("Сервер запущен на :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
+// Получить все задачи
 func GetAllTasksHandler(w http.ResponseWriter, r *http.Request) {
 	tasks, err := tasks.GetAllTasks(db)
 	if err != nil {
@@ -36,6 +44,25 @@ func GetAllTasksHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
+func GetTaskByIDHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Некорректный ID задачи", http.StatusBadRequest)
+		return
+	}
+
+	task, err := tasks.GetTaskByID(db, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+// Добавить задачу
 func AddTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var t tasks.Task
 	err := json.NewDecoder(r.Body).Decode(&t)
@@ -52,4 +79,51 @@ func AddTaskHandler(w http.ResponseWriter, r *http.Request) {
 	t.Id = id
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(t)
+}
+
+// Обновить задачу
+func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	// Получение параметра {id} из URL
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Некорректный ID задачи", http.StatusBadRequest)
+		return
+	}
+
+	var t tasks.Task
+	err = json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Неверный формат данных", http.StatusBadRequest)
+		return
+	}
+
+	// Обновляем задачу в базе данных
+	err = tasks.UpdateTask(db, id, t.Status, t.Description)
+	if err != nil {
+		http.Error(w, "Ошибка при обновлении задачи", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Удалить задачу
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	// Получение параметра {id} из URL
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Некорректный ID задачи", http.StatusBadRequest)
+		return
+	}
+
+	// Удаляем задачу из базы данных
+	err = tasks.DeleteTask(db, id)
+	if err != nil {
+		http.Error(w, "Ошибка при удалении задачи", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
